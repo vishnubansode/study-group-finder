4640c3d3da8226ef6198cf23e831ce21ec213661
sarran528 <sarranm528@gmail.com>
2025-10-01 19:07:40 +0530

    cleaned main

--- src/main/java/com/groupgenius/groupgenius_backend/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.groupgenius.groupgenius_backend.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.List;
-//
-//public interface CourseRepository extends JpaRepository<Course, Long> {
-//    List<Course> findByCourseNameContainingIgnoreCase(String name);
-//}
